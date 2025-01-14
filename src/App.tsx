b8a0b44e11cca51c844803ba29c457ce6c78ef26
Randeepa Jayasekara <147414352+randeepajayasekara@users.noreply.github.com>
2025-01-14 18:15:05 +0530

    Header Design Finalize

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
 import { Outlet, Link } from "react-router-dom";
+import '@fontsource/poppins/100.css';
+import '@fontsource/poppins/200.css';
+import '@fontsource/poppins/300.css';
+import '@fontsource/poppins/400.css';
+import '@fontsource/poppins/500.css';
+import '@fontsource/poppins/600.css';
+import '@fontsource/poppins/700.css';
+import '@fontsource/poppins/800.css';
+import '@fontsource/poppins/900.css';
 
 import { useState } from "react";
 import {
@@ -13,55 +22,50 @@ import {
   PopoverPanel,
 } from "@headlessui/react";
 import {
-  ArrowPathIcon,
+  MegaphoneIcon,
   Bars3Icon,
-  ChartPieIcon,
-  CursorArrowRaysIcon,
-  FingerPrintIcon,
-  SquaresPlusIcon,
+  RocketLaunchIcon,
+  GlobeAsiaAustraliaIcon,
+  WindowIcon,
   XMarkIcon,
 } from "@heroicons/react/24/outline";
 import {
   ChevronDownIcon,
-  PhoneIcon,
-  PlayCircleIcon,
+  UserGroupIcon,
+  EnvelopeIcon,
 } from "@heroicons/react/20/solid";
+
+import { ModeToggle } from "./components/theme/mode-toggle";
 
 const products = [
   {
-    name: "Analytics",
-    description: "Get a better understanding of your traffic",
+    name: "Experience",
+    description: "Navigate the Digital World Confidently.",
     href: "#",
-    icon: ChartPieIcon,
+    icon: RocketLaunchIcon,
   },
   {
-    name: "Engagement",
-    description: "Speak directly to your customers",
+    name: "Problem Statement",
+    description: "Explore growing cybersecurity threats faced by Sri Lankans.",
     href: "#",
-    icon: CursorArrowRaysIcon,
+    icon: GlobeAsiaAustraliaIcon,
   },
   {
-    name: "Security",
-    description: "Your customers’ data will be safe and secure",
+    name: "Overview",
+    description: "Quick summary of what users can expect to find on the website",
     href: "#",
-    icon: FingerPrintIcon,
+    icon: WindowIcon,
   },
   {
-    name: "Integrations",
-    description: "Connect with third-party tools",
+    name: "Call to Action",
+    description: "Explore and Access Resources",
     href: "#",
-    icon: SquaresPlusIcon,
-  },
-  {
-    name: "Automations",
-    description: "Build strategic funnels that will convert",
-    href: "#",
-    icon: ArrowPathIcon,
+    icon: MegaphoneIcon,
   },
 ];
 const callsToAction = [
-  { name: "Watch demo", href: "#", icon: PlayCircleIcon },
-  { name: "Contact sales", href: "#", icon: PhoneIcon },
+  { name: "Feedback", href: "#", icon: EnvelopeIcon },
+  { name: "About sUs", href: "#", icon: UserGroupIcon },
 ];
 
 function App() {
@@ -69,7 +73,7 @@ function App() {
 
   return (
     <>
-      <header className="bg-white/30 backdrop-blur-lg ">
+      <header className="bg-white/30 dark:bg-black/10 backdrop-blur-lg fixed inset-x-0 top-0 lg:z-20 text-black dark:text-white">
         <nav
           aria-label="Global"
           className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
@@ -79,7 +83,7 @@ function App() {
               <span className="sr-only">Crypticus Vigilance Lanka</span>
               <img
                 alt=""
-                src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+                src="https://icons.iconarchive.com/icons/paomedia/small-n-flat/512/shield-ok-icon.png"
                 className="h-8 w-auto"
               />
             </Link>
@@ -90,13 +94,13 @@ function App() {
               onClick={() => setMobileMenuOpen(true)}
               className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
             >
-              <span className="sr-only">Open main menu</span>
+              <span className="sr-only">Menu</span>
               <Bars3Icon aria-hidden="true" className="size-6" />
             </button>
           </div>
           <PopoverGroup className="hidden lg:flex lg:gap-x-12">
             <Popover className="relative">
-              <PopoverButton className="flex items-center gap-x-1 text-sm/6 font-semibold text-gray-900">
+              <PopoverButton className="flex items-center gap-x-1 text-sm/6 font-semibold text-gray-900 dark:text-gray-100">
                 <Link to="/">
                   Home
                 </Link>
@@ -108,13 +112,13 @@ function App() {
 
               <PopoverPanel
                 transition
-                className="absolute top-full -left-8 z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white ring-1 shadow-lg ring-gray-900/5 transition data-closed:translate-y-1 data-closed:opacity-0 data-enter:duration-200 data-enter:ease-out data-leave:duration-150 data-leave:ease-in"
+                className="absolute top-full -left-8 z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white dark:bg-zinc-950 ring-1 shadow-lg ring-gray-900/5 transition data-closed:translate-y-1 data-closed:opacity-0 data-enter:duration-200 data-enter:ease-out data-leave:duration-150 data-leave:ease-in"
               >
                 <div className="p-4">
                   {products.map((item) => (
                     <div
                       key={item.name}
-                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm/6 hover:bg-gray-50"
+                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm/6 hover:bg-gray-50 dark:hover:bg-gray-800/50"
                     >
                       <div className="flex size-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
                         <item.icon
@@ -125,22 +129,22 @@ function App() {
                       <div className="flex-auto">
                         <a
                           href={item.href}
-                          className="block font-semibold text-gray-900"
+                          className="block font-semibold text-gray-900 dark:text-white"
                         >
                           {item.name}
                           <span className="absolute inset-0" />
                         </a>
-                        <p className="mt-1 text-gray-600">{item.description}</p>
+                        <p className="mt-1 text-gray-600 dark:text-gray-300">{item.description}</p>
                       </div>
                     </div>
                   ))}
                 </div>
-                <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
+                <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50 dark:bg-zinc-900">
                   {callsToAction.map((item) => (
                     <a
                       key={item.name}
                       href={item.href}
-                      className="flex items-center justify-center gap-x-2.5 p-3 text-sm/6 font-semibold text-gray-900 hover:bg-gray-100"
+                      className="flex items-center justify-center gap-x-2.5 p-3 text-sm/6 font-semibold text-gray-900 dark:text-gray-200 hover:bg-gray-100"
                     >
                       <item.icon
                         aria-hidden="true"
@@ -154,25 +158,23 @@ function App() {
             </Popover>
 
             <Link to="/security-tips">
-              <span className="text-sm/6 font-semibold text-gray-900">
+              <span className="text-sm/6 font-normal text-gray-900 dark:text-gray-100">
                 Tips
               </span>
             </Link>
             <Link to="/awareness-local">
-              <span className="text-sm/6 font-semibold text-gray-900">
+              <span className="text-sm/6 font-semibold text-gray-900 dark:text-gray-100">
                 Aware
               </span>
             </Link>
             <Link to="/resource-tools">
-              <span className="text-sm/6 font-semibold text-gray-900">
+              <span className="text-sm/6 font-semibold text-gray-900 dark:text-gray-100">
                 Resources
               </span>
             </Link>
           </PopoverGroup>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="#" className="text-sm/6 font-semibold text-gray-900">
-              Log in <span aria-hidden="true">&rarr;</span>
-            </a>
+            <ModeToggle />
           </div>
         </nav>
         <Dialog
@@ -181,16 +183,16 @@ function App() {
           className="lg:hidden"
         >
           <div className="fixed inset-0 z-10" />
-          <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white/30 backdrop-blur-md px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white/30 dark:bg-black/10 backdrop-blur-lg px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
-              <a href="#" className="-m-1.5 p-1.5">
+              <Link to="/" className="-m-1.5 p-1.5">
                 <span className="sr-only">Crypticus Vigilance Lanka</span>
                 <img
                   alt=""
-                  src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+                  src="https://icons.iconarchive.com/icons/paomedia/small-n-flat/512/shield-ok-icon.png"
                   className="h-8 w-auto"
                 />
-              </a>
+              </Link>
               <button
                 type="button"
                 onClick={() => setMobileMenuOpen(false)}
@@ -204,7 +206,7 @@ function App() {
               <div className="-my-6 divide-y divide-gray-500/10">
                 <div className="space-y-2 py-6">
                   <Disclosure as="div" className="-mx-3">
-                    <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pr-3.5 pl-3 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
+                    <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pr-3.5 pl-3 text-base/7 font-semibold text-gray-900 dark:text-gray-100 hover:bg-gray-50 dark:hover:bg-gray-800/50">
                       <Link to="/">
                         Home
                       </Link>
@@ -219,7 +221,7 @@ function App() {
                           key={item.name}
                           as="a"
                           href={item.href}
-                          className="block rounded-lg py-2 pr-3 pl-6 text-sm/7 font-semibold text-gray-900 hover:bg-gray-50"
+                          className="block rounded-lg py-2 pr-3 pl-6 text-sm/7 font-semibold text-gray-900 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800/50"
                         >
                           {item.name}
                         </DisclosureButton>
@@ -228,30 +230,29 @@ function App() {
                   </Disclosure>
                   <Link
                     to="/security-tips"
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50 dark:text-gray-100 dark:hover:bg-gray-800/50"
                   >
                     Tips
                   </Link>
                   <Link
                     to="/awareness-local"
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50 dark:text-gray-100 dark:hover:bg-gray-800/50"
                   >
                     Aware
                   </Link>
                   <Link
                     to="/resource-tools"
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50 dark:text-gray-100 dark:hover:bg-gray-800/50"
                   >
                     Resources
                   </Link>
                 </div>
                 <div className="py-6">
-                  <a
-                    href="#"
-                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                  <div
+                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 text-white"
                   >
-                    Log in
-                  </a>
+                    <ModeToggle />
+                  </div>
                 </div>
               </div>
             </div>
--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './index.css'
-import App from './App.tsx'
-import Home from './routes/index.tsx'
-import ResourceToolsPage from './routes/resource-tools.tsx'
-import SecurityTipsPage from './routes/security-tips.tsx'
-import AwarenessLocalPage from './routes/awareness-local.tsx'
-import NotFound from './routes/error/NotFound.tsx'
+import '@/index.css'
+import App from '@/App.tsx'
+import Home from '@/routes/index.tsx'
+import ResourceToolsPage from '@/routes/resource-tools.tsx'
+import SecurityTipsPage from '@/routes/security-tips.tsx'
+import AwarenessLocalPage from '@/routes/awareness-local.tsx'
+import NotFound from '@/routes/error/NotFound.tsx'
+import { ThemeProvider } from "@/components/theme/theme-provider.tsx"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="resource-tools" element={<ResourceToolsPage />} />
-          <Route path="security-tips" element={<SecurityTipsPage />} />
-          <Route path="awareness-local" element={<AwarenessLocalPage />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<Home />} />
+            <Route path="resource-tools" element={<ResourceToolsPage />} />
+            <Route path="security-tips" element={<SecurityTipsPage />} />
+            <Route path="awareness-local" element={<AwarenessLocalPage />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </ThemeProvider>
     </BrowserRouter>
   </StrictMode>,
 )
